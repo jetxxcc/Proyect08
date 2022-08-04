d5374d1874366e432e504a690ebbf64842b60482
jetxxcc <jesusdotel91@gmail.com>
2022-08-04 00:50:27 -0400

    actualizacion 2

--- javaProyect/src/windows/op.java
+++ javaProyect/src/windows/op.java
@@ -7,10 +7,18 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import java.awt.Window.Type;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JSeparator;
+import javax.swing.SwingConstants;
 
 public class op extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -32,15 +40,48 @@ public class op extends JFrame {
 	 * Create the frame.
 	 */
 	public op() {
+		setType(Type.UTILITY);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("New label");
-		contentPane.add(lblNewLabel, BorderLayout.NORTH);
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.DARK_GRAY);
+		
+		panel.setBounds(5, 5, 424, 28);
+		contentPane.add(panel);
+		
+		JLabel lblNewLabel = new JLabel("Login");
+		lblNewLabel.setFont(new Font("Verdana", Font.PLAIN, 14));
+		lblNewLabel.setForeground(Color.LIGHT_GRAY);
+		panel.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Usuario");
+		lblNewLabel_1.setBounds(232, 77, 46, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Contraseña");
+		lblNewLabel_2.setBounds(232, 133, 72, 14);
+		contentPane.add(lblNewLabel_2);
+		
+		textField = new JTextField();
+		textField.setBounds(298, 74, 107, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(298, 130, 107, 20);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBackground(Color.GRAY);
+		separator.setForeground(Color.LIGHT_GRAY);
+		separator.setOrientation(SwingConstants.VERTICAL);
+		separator.setBounds(209, 44, 13, 206);
+		contentPane.add(separator);
 	}
-
 }
