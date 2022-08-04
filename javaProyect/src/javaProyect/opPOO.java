cf6d14222029f42d157f2d69bb93f64c481b7f95
jetxxcc <jesusdotel91@gmail.com>
2022-08-04 01:16:28 -0400

    agrege una clase nueva

---
+++ javaProyect/src/javaProyect/opPOO.java
@@ -0,0 +1,26 @@
+package javaProyect;
+
+public class opPOO {
+	 String user, contra;
+	
+	 opPOO(String user, String contra){
+		 this.user=user;
+		 this.contra=contra;
+	 }
+	 
+	 void setUser(String x) {
+		 this.user=x;
+	 }
+	 
+	 void setContra(String y) {
+		 this.contra=y;
+	 }
+	 
+	 String getUser() {
+		 return user;
+	 }
+	 String getContra() {
+		 return contra;
+	 }
+
+}
